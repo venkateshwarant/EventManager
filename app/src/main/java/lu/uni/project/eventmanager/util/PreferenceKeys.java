f09621a6cf89ca80f07ca3f6d43a2cf91c497f2e
Venkateshwaran Thamilselvan <venkateshwarant@gmail.com>
2019-11-04 16:41:06 +0100

    [Feature] SharedPreference Helper class

--- app/src/main/java/lu/uni/project/eventmanager/util/PreferenceKeys.java
+++ app/src/main/java/lu/uni/project/eventmanager/util/PreferenceKeys.java
@@ -1,4 +1,10 @@
 package lu.uni.project.eventmanager.util;
 
 public class PreferenceKeys {
+
+    public static String profilePhotoURI= "profile.photo_uri";
+    public static String profileFirstName= "profile.first_name";
+    public static String profileLastName= "profile.last_name";
+    public static String profileDisplayName= "profile.display_name";
+
 }
