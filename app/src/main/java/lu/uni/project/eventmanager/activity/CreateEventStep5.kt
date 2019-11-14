150600c9f0608a989a5c157957ae3062e0a58839
Venkateshwaran Thamilselvan <venkateshwarant@gmail.com>
2019-11-14 23:08:05 +0100

    [Feature] Create events

---
+++ app/src/main/java/lu/uni/project/eventmanager/activity/CreateEventStep5.kt
@@ -0,0 +1,12 @@
+package lu.uni.project.eventmanager.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateEventStep5 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_event_step5)
+    }
+}
