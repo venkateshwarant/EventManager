93fa4f39a1d9821f4bc9810b9c32cde4593bffea
Vanitha Varadharajan <vanitha2013v@gmail.com>
2019-11-14 23:17:29 +0100

    [Enhancement] Create events

--- app/src/main/java/lu/uni/project/eventmanager/pojo/Location.java
+++ app/src/main/java/lu/uni/project/eventmanager/pojo/Location.java
@@ -1,4 +1,62 @@
 package lu.uni.project.eventmanager.pojo;
 
-class Location {
+import android.os.Parcel;
+
+import java.io.Serializable;
+
+public class Location implements Serializable {
+    public String latitude="";
+    public String longitude="";
+    public String address="";
+    public String zipCode="";
+
+    public Location() {
+    }
+
+    public Location(String latitude, String longitude, String address, String zipCode) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.address = address;
+        this.zipCode = zipCode;
+    }
+
+    public Location(Parcel in) {
+        latitude = in.readString();
+        longitude = in.readString();
+        address = in.readString();
+        zipCode = in.readString();
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
 }
--- app/src/main/java/lu/uni/project/eventmanager/util/BundleKeys.kt
+++ app/src/main/java/lu/uni/project/eventmanager/util/BundleKeys.kt
@@ -1,4 +1,20 @@
 package lu.uni.project.eventmanager.util
 
 class BundleKeys {
+    companion object{
+        var event= "event"
+        var eventNameKey= "event_name"
+        var eventDescriptionKey= "event_description"
+        var eventCategoryKey= "event_category"
+        var imagesListKey= "images_list"
+        var startDateKey= "start_date"
+        var endDateKey= "end_date"
+        var startTimeKey= "start_time"
+        var endTimeKey= "end_time"
+        var addressKey= "address"
+        var lattitudeKey= "lattitude"
+        var longitudeKey= "longitude"
+        var zipcodeKey= "zipcode"
+
+    }
 }
