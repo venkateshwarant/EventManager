3e41b67acf86dbaa86c0832fb45535c9215849cc
Vanitha Varadharajan <vanitha2013v@gmail.com>
2019-11-14 23:16:06 +0100

    [Enhancement] Create event

--- app/src/main/java/lu/uni/project/eventmanager/pojo/Event.java
+++ app/src/main/java/lu/uni/project/eventmanager/pojo/Event.java
@@ -1,4 +1,133 @@
 package lu.uni.project.eventmanager.pojo;
 
-public class Event {
+import java.io.Serializable;
+
+public class Event implements Serializable {
+    public String eventName="";
+    public String eventDescription="";
+    public String eventCategory="";
+//    public List<Uri> images= Collections.emptyList();
+    public String images="";
+    public String startDate="";
+    public String endDate="";
+    public String startTime="";
+    public String endTime="";
+    public String timeZone="";
+    public String createdTime="";
+    public String eventId="";
+    public String userId="";
+    public Location location= new Location();
+    public int imagesCount= 0;
+    public Event() {
+    }
+
+    public Event(String eventName, String eventDescription, String eventCategory, String startDate, String endDate, String startTime, String endTime, String timeZone, Location location) {
+        this.eventName = eventName;
+        this.eventDescription = eventDescription;
+        this.eventCategory = eventCategory;
+//        this.images = images;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.timeZone = timeZone;
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventDescription() {
+        return eventDescription;
+    }
+
+    public void setEventDescription(String eventDescription) {
+        this.eventDescription = eventDescription;
+    }
+
+    public String getEventCategory() {
+        return eventCategory;
+    }
+
+    public void setEventCategory(String eventCategory) {
+        this.eventCategory = eventCategory;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+
+
+    public String getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(String createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
